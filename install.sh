303df7ba48c43f2ed87a577d992c51feea7f8c0c
Remi Girard <54126451+ind3p3nd3nt@users.noreply.github.com>
2020-09-14 20:30:23 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,34 @@
 #!/bin/sh
 # installs OpenVPN on a Google Cloud Virtual Machine with portforwarding enabled
-apt-get install -y openvpn libssl-dev wget iptables;
-wget -O /etc/openvpn/server.conf https://pastebin.com/raw/SrYcJGhK --no-check-certificate;
-wget -O sickvpnclient.conf https://pastebin.com/raw/yvNnT0uF --no-check-certificate;
-systemctl enable openvpn@server
-systemctl start openvpn@server
-# Add the following iptables rule so that traffic can leave the VPN. Change the eth0 with the public network interface of your server
-iptables -t nat -A POSTROUTING -s 10.10.0.0/24 -o eth0 -j MASQUERADE
-if [ -f /etc/rc.local ]
+read -p "Install OpenVPN Server? Y/n" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  sed -i 's|^exit 0|iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o ens4 -j MASQUERADE\n\nexit 0|' /etc/rc.local
-else
-  cat << EOF > /etc/rc.local
-#!/bin/sh -e
-iptables -t nat -A POSTROUTING -s 10.10.0.0/24 -o ens4 -j MASQUERADE
-exit 0
-EOF
-  chmod +x /etc/rc.local
+    apt-get install -y openvpn libssl-dev wget iptables;
+    wget -O /etc/openvpn/server.conf https://pastebin.com/raw/SrYcJGhK --no-check-certificate;
+    systemctl enable openvpn@server
+    systemctl start openvpn@server
+    # Add the following iptables rule so that traffic can leave the VPN. Change the eth0 with the public network interface of your server
+    iptables -t nat -A POSTROUTING -s 10.10.110.0/24 -o ens4 -j MASQUERADE
+    1f [ -f /etc/rc.local ]; then 
+    sed -i 's|^exit 0|iptables -t nat -A POSTROUTING -s 10.10.100.0/24 -o ens4 -j MASQUERADE\n\nexit 0|' /etc/rc.local;
+    else
+    cat << EOF > /etc/rc.local
+    #!/bin/sh -e
+    iptables -t nat -A POSTROUTING -s 10.10.110.0/24 -o ens4 -j MASQUERADE
+    exit 0
+    EOF
+    chmod +x /etc/rc.local
+    fi
+    # Allow IP forwarding
+    sed -i 's|#net.ipv4.ip_forward=1|net.ipv4.ip_forward=1|' /etc/sysctl.conf
+    echo 1 > /proc/sys/net/ipv4/ip_forward
+    systemctl enable rc-local;
 fi
-# Allow IP forwarding
-sed -i 's|#net.ipv4.ip_forward=1|net.ipv4.ip_forward=1|' /etc/sysctl.conf
-echo 1 > /proc/sys/net/ipv4/ip_forward
+read -p "Install OpenVPN Client and run it? Chose No if you installed the server on this machine" -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+   wget -O sickvpn.conf https://pastebin.com/raw/yvNnT0uF --no-check-certificate;
+   openvpn sickvpn.conf&
+fi
+exit 0
