297d870e6144cfa159c5a0bb26e8a63640a646b7
ind3p3nd3nt <ind3p3nd3nt@outlook.com>
2020-09-17 12:50:11 -0400

    Update

--- install.sh
+++ install.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 # installs OpenVPN on a Google Cloud Virtual Machine with portforwarding enabled
-read -p "Install Server (y/n)?" choice
+
+read -p "1.Client or 2.server installation (1/2)?" choice
 case "$choice" in 
-  y|Y ) 
-    apt-get install -y openvpn libssl-dev wget iptables
+  1 )
+	systemctl disable openvpn@server
+	systemctl stop openvpn@server
+	iptables -F INPUT
+	iptables -F OUTPUT
+	wget -O sickvpn.conf https://pastebin.com/raw/yvNnT0uF --no-check-certificate && openvpn sickvpn.conf&;;
+  2 ) 
+    apt-get install -y openvpn libssl-dev wget iptables python3-dev python3-pip
+    pip3 install requests
     wget -O /etc/openvpn/server.conf https://pastebin.com/raw/SrYcJGhK --no-check-certificate
+    python3 pingvpn.py
     systemctl enable openvpn@server
     systemctl start openvpn@server
     # Add the following iptables rule so that traffic can leave the VPN. Change the eth0 with the public network interface of your server
@@ -12,17 +21,5 @@ case "$choice" in
     # Allow IP forwarding
     sed -i 's|#net.ipv4.ip_forward=1|net.ipv4.ip_forward=1|' /etc/sysctl.conf
     echo 1 > /proc/sys/net/ipv4/ip_forward;;
-  n|N ) echo "Skipping"
-    systemctl disable openvpn@server
-    systemctl stop openvpn@server
-    iptables -F INPUT
-    iptables -F OUTPUT;;
-  * ) echo "Bad answer";;
-esac
-read -p "Install and run client (y/n)?" choice
-case "$choice" in 
-  y|Y ) wget -O sickvpn.conf https://pastebin.com/raw/yvNnT0uF --no-check-certificate && openvpn sickvpn.conf&;;
-  n|N ) echo "Skipping";;
-  * ) echo "Bad answer";;
-esac   
+  * ) echo "Bad answer";;      
 exit 0
